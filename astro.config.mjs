77607ec178be436ee8faddcbaf60c372a15b86fb
Julien Muetton <julien@muetton.me>
2024-04-30 14:49:45 +0200

    Add deploy to gh-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind()]
+  site: "https://julien.muetton.me",
+  integrations: [mdx(), tailwind()],
 });
+

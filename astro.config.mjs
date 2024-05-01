cf1ddd884b797e4ba0d5f89ec4a249f267b3da3a
Julien Muetton <julien@muetton.me>
2024-05-01 23:01:15 +0200

    Add a way to render custom markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import markdownIntegration from "@astropub/md";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://julien.muetton.me",
-  integrations: [mdx(), tailwind()],
+  integrations: [mdx(), tailwind(), markdownIntegration()],
 });
-

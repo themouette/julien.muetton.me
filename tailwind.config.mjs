023ca05c07bbdfebae90eb52863581449304fe82
Julien Muetton <julien@muetton.me>
2024-05-01 11:17:42 +0200

    Define some theme colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        text: {
+          primary: "rgba(0,0,0, .76)",
+          secondary: "rgba(255,255,255, .54)",
+        },
+        border: {
+          primary: "#e2e5ed",
+          secondary: "#2c2b2b",
+        },
+        background: {
+          primary: "#ffffff",
+          secondary: "rgba(0,0,0, .76)",
+        },
+      },
+    },
+  },
+  plugins: [],
+};

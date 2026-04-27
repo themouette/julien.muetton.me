2b23c4a755e9db5425dca570e2748eed7ca93580
Julien Muetton <julien@muetton.me>
2026-04-27 23:34:49 +0200

    update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import markdownIntegration from "@astropub/md";
-
-import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://julien.muetton.me",
-  integrations: [mdx(), tailwind(), markdownIntegration()],
+  integrations: [mdx()],
 });
